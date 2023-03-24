a003906c7acc098456a6792e68b2d7c546053d9e
Eric Oliveira <oiericoi@hotmail.com>
2023-03-24 15:51:46 -0300

    refactor: modify post card component

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,18 +1,26 @@
 import { useEffect, useState } from "react"
 import { postsService } from "../../service/posts"
+import { usersService } from "../../service/users"
 
 import './style.css'
 
 const PostCard = () => {
   const [ posts, setPosts ] = useState()
+  const [ users, setUsers ] = useState()
 
   const getPosts = async () => {
     let result = await postsService.readAll()
     setPosts(result)
   }
-  
+
+  const getUsers = async () => {
+    let result = await usersService.readAll()
+    setUsers(result)
+  }
+
   useEffect(() => {
     getPosts()
+    getUsers()
   }, [])
 
   return (
@@ -25,6 +33,17 @@ const PostCard = () => {
           return (
             <div className="posts" key={post.id}>
               <h2 className="post-title">{post.title}</h2>
+              {!users ? (
+                ''
+              ) : (
+                users.map(user => {
+                  if (user.id === post.userId) {
+                    return (
+                      <p key={user.id}>Autor: <a href={`/user/${user.id}`}>{user.name}</a></p>
+                    )
+                  }
+                })
+              )}
               <p className="post-p">{post.body}</p>
               <a className="post-link" href={`/post/${post.id}`}>ver comentários</a>
             </div>
