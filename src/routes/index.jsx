42573d45fafa70da3490e141bfa93b782c574b34
Eric Oliveira <oiericoi@hotmail.com>
2023-03-24 15:54:36 -0300

    style: add user card component in routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PostCard from '../components/PostCard';
 import PostComents from '../components/PostComments';
+import UserCard from '../components/UserCard';
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
         <Routes>
           <Route exact path="/" element={<PostCard />} />
           <Route exact path="/post/:id" element={<PostComents />} />
+          <Route exact path="/user/:id" element={<UserCard />} />
         </Routes>
       </BrowserRouter>
     </>
