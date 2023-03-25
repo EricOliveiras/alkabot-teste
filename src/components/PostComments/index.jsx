a406a5ef186feadb839bec78102ac8641be11ad6
Eric Oliveira <oiericoi@hotmail.com>
2023-03-25 13:16:22 -0300

    refactor: modify post comments component

--- src/components/PostComments/index.jsx
+++ src/components/PostComments/index.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { postsService } from "../../service/posts"
+import { usersService } from "../../service/users"
 
 import './style.css'
 
 const PostComments = () => {
   const [ comments, setComments ] = useState()
   const [ post, setPost ] = useState()
+  const [ users, setUsers ] = useState()
 
   const { id } = useParams()
 
@@ -20,20 +22,37 @@ const PostComments = () => {
     setComments(result)
   }
 
+  const getUser = async () => {
+    let result = await usersService.readAll()
+    setUsers(result)
+  }
+
   useEffect(() => {
     getPost()
     getComments()
+    getUser()
   }, [])
 
   return (
     <div className="container">
-      <p><a href="/">voltar</a></p>
+      <p><a href="javascript:history.back()">voltar</a></p>
       {!post ? (
         <p>Post não encontrado!</p>
       ) : (
         <>
           <div className="posts" key={post.id}>
             <h2 className="post-title">{post.title}</h2>
+            {!users ? (
+              ''
+            ) : (
+              users.map(user => {
+                if (post.userId === user.id) {
+                  return (
+                    <p className="user-p-link" key={user.id}>Autor: <a href={`/user/${user.id}`}>{user.name}</a></p>
+                  )
+                }
+              })
+            )}
             <p className="post-p">{post.body}</p>
           </div>
           <h2>Comentários: </h2>
